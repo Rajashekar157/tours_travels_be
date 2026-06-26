ef7a96637e5aab898f52e6fdcaed1908285c9005
Rajashekar157 <rajashekarmaddi562@gmail.com>
2026-06-26 21:57:24 +0530

    adededed

--- app/main.py
+++ app/main.py
@@ -17,7 +17,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "https://tours-travels-1.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
